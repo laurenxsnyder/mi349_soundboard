6544b06dd8ded37c32a9ab234c8ae5822989b79a
Lauren Snyder <laurenxsnyder@gmail.com>
2022-11-20 23:30:31 -0500

    buttons

--- soundboard.js
+++ soundboard.js
@@ -1,10 +1,27 @@
-<figure>
-    <figcaption>Listen to the T-Rex:</figcaption>
-    <audio
-        controls
-        src="bubbles_sfx.wav">
-            <a href="bubbles_sfx.wav">
-                Download audio
-            </a>
-    </audio>
-</figure>
+var playBtn = document.getElementById('play'),
+  resetBtn = document.getElementById('reset'),
+  hearbeat = document.getElementById('heartbeat')
+	audios = document.querySelectorAll('audio');
+console.log(audios);
+
+
+playBtn.addEventListener('mouseover', function() {
+[].forEach.call(audios, function(audio) {
+  // do whatever
+  audio.play();
+});
+}, false);
+
+playBtn.addEventListener('mouseleave', function() {
+  heartbeat.pause();
+  heartbeat.currentTime = 0;
+}, false);
+
+resetBtn.addEventListener('mouseover', function() {
+    heartbeat.play();
+}, false);
+
+resetBtn.addEventListener('mouseleave', function() {
+  heartbeat.pause();
+  heartbeat.currentTime = 0;
+}, false);
