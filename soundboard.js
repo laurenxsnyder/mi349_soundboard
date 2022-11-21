afdfd3ba2e9d07908dad6d4812f3e9bcab79e32c
Lauren Snyder <laurenxsnyder@gmail.com>
2022-11-21 00:44:16 -0500

    add sound

--- soundboard.js
+++ soundboard.js
@@ -2,3 +2,10 @@
 document.getElementById("horn_button").addEventListener("click",function(){
     document.getElementById("horn_audio").play()}
 );
+
+document.getElementById("bubbles_button").addEventListener("click",function(){
+    document.getElementById("bubbles_audio").play()}
+);
+document.getElementById("cuckoo_button").addEventListener("click",function(){
+    document.getElementById("cuckoo_audio").play()}
+);
