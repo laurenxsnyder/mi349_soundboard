d2a4017c465d489c07eb7c72f40f1f71c89748fc
Lauren Snyder <laurenxsnyder@gmail.com>
2022-11-21 00:13:04 -0500

    sound button

--- soundboard.js
+++ soundboard.js
@@ -1,27 +1,3 @@
-var playBtn = document.getElementById('play'),
-  resetBtn = document.getElementById('reset'),
-  hearbeat = document.getElementById('bubbles')
-	audios = document.querySelectorAll('audio');
-console.log(audios);
-
-
-playBtn.addEventListener('mouseover', function() {
-[].forEach.call(audios, function(audio) {
-  // do whatever
-  audio.play();
-});
-}, false);
-
-playBtn.addEventListener('mouseleave', function() {
-  bubbles.pause();
-  bubbles.currentTime = 0;
-}, false);
-
-resetBtn.addEventListener('mouseover', function() {
-    bubbles.play();
-}, false);
-
-resetBtn.addEventListener('mouseleave', function() {
-  bubbles.pause();
-  bubbles.currentTime = 0;
-}, false);
+function playMyAudio(){
+    document.getElementById("myAudio").play();
+  }
